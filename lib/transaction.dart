abe49f4c3c1f59be0764404d1293b1ec8a517cfa
matheustkaczyk <matheus111904@hotmail.com>
2023-01-12 15:03:26 -0300

    feat: creating the Transaction object

---
+++ lib/transaction.dart
@@ -0,0 +1,12 @@
+class Transaction {
+  int id;
+  String title;
+  double value;
+  DateTime date;
+
+  Transaction(
+      {required this.id,
+      required this.title,
+      required this.value,
+      required this.date}) {}
+}
