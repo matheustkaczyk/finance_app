46d52a57c35525dd7e376b17e86aacfe40285cc9
matheustkaczyk <matheus111904@hotmail.com>
2023-01-16 11:23:43 -0300

    refact: deleting user_transaction, bringing back stuff to main.dart, needed the context object

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,85 @@
+import 'package:finance_app/widgets/new_transaction.dart';
 import 'package:finance_app/widgets/transaction_list.dart';
-import 'package:finance_app/widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
 
+import 'models/transaction.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Minhas finanças'),
-        ),
-        body: SingleChildScrollView(
-          child: UserTransactions(),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(textTitleController, textValueController,
+              _createTransactionClick);
+        });
+  }
+
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: 1,
+        title: 'Novo tênis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 2, title: 'Conta de luz', value: 211.30, date: DateTime.now()),
+  ];
+
+  void _createTransactionClick() {
+    try {
+      String titleValue = textTitleController.text;
+      double value = double.parse(textValueController.text);
+
+      if (titleValue.isEmpty || value <= 0) return;
+
+      return setState(() {
+        _transactions.add(Transaction(
+            id: _transactions.length + 1,
+            title: titleValue,
+            value: value,
+            date: DateTime.now()));
+      });
+    } catch (_) {
+      return;
+    }
+  }
+
+  final textTitleController = TextEditingController();
+
+  final textValueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minhas finanças'),
+      ),
+      body: SingleChildScrollView(
+        child: TransactionsList(_transactions),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
