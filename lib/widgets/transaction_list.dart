e1bdd1e2d3e9fa25fe708cc43e61c85056a10338
matheustkaczyk <matheus111904@hotmail.com>
2023-01-17 15:17:19 -0300

    fix: re-implementing dismissible logic on transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -29,45 +29,53 @@ class _TransactionsListState extends State<TransactionsList> {
               itemCount: widget.transactions.length,
               itemBuilder: (context, index) {
                 final transaction = widget.transactions[index];
-                return Card(
-                  color: Colors.lightBlue,
-                  margin: const EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Text(
-                          "R\$ ${transaction.value.toStringAsFixed(2)}",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                              color: Colors.amberAccent),
-                        ),
-                      ),
-                      Container(
+                return Dismissible(
+                  key: Key(widget.transactions[index].toString()),
+                  onDismissed: (direction) => {
+                    setState(() {
+                      widget.transactions.removeAt(index);
+                    })
+                  },
+                  child: Card(
+                    color: Colors.lightBlue,
+                    margin: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
                           padding: const EdgeInsets.only(left: 15),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                transaction.title,
-                                style: const TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                              Text(
-                                dateFormatter(transaction.date.toString()),
-                                style: const TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w300),
-                              ),
-                            ],
-                          )),
-                    ],
+                          child: Text(
+                            "R\$ ${transaction.value.toStringAsFixed(2)}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.amberAccent),
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  transaction.title,
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                Text(
+                                  dateFormatter(transaction.date.toString()),
+                                  style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            )),
+                      ],
+                    ),
                   ),
                 );
               },
