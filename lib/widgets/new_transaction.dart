ffb218c9e7e0a0525864e238d5c89a61f8d94588
matheustkaczyk <matheus111904@hotmail.com>
2023-01-19 15:47:32 -0300

    feat: adding fields to select the transaction date

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -17,41 +17,48 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        height: 180,
-        padding: EdgeInsets.all(2),
-        child: Card(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
+    return Container(
+      height: 300,
+      padding: EdgeInsets.all(10),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: "Título"),
+              controller: widget.textTitleController,
+              onSubmitted: (_) => {},
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: "Valor"),
+              controller: widget.textValueController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
+              ],
+              onSubmitted: (_) => widget.createBtnClick(),
+            ),
+            Row(
               children: [
-                TextField(
-                  decoration: const InputDecoration(labelText: "Título"),
-                  controller: widget.textTitleController,
-                  onSubmitted: (_) => {},
-                ),
-                TextField(
-                  decoration: const InputDecoration(labelText: "Valor"),
-                  controller: widget.textValueController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                        RegExp(r'^\d+\.?\d{0,2}')),
-                  ],
-                  onSubmitted: (_) => widget.createBtnClick(),
-                ),
+                Text("Nenhuma data selecionada!"),
                 TextButton(
-                  onPressed: widget.createBtnClick,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.green),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                  onPressed: () {},
+                  child: const Text(
+                    "Selecionar Data",
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  child: const Text("Criar"),
                 )
-              ]),
-        ),
-      ),
+              ],
+            ),
+            TextButton(
+              onPressed: widget.createBtnClick,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+              ),
+              child: const Text("Criar"),
+            )
+          ]),
     );
   }
 }
