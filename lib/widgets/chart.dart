96cee2fb56e88ffc9cf8261ae03f701cee92deca
matheustkaczyk <matheus111904@hotmail.com>
2023-01-19 10:33:46 -0300

    feat: starting to implement the chart widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,35 @@
+import 'package:finance_app/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions) {}
+
+  List<Map<String, Object>> get groupedTransactionsValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (int i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'value': totalSum};
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card();
   }
