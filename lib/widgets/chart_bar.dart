af43e780a2443a99d1a065292d69a76e90e4dc02
matheustkaczyk <matheus111904@hotmail.com>
2023-01-19 11:21:17 -0300

    feat: adding padding around the chart_bar, adding fittedbox around text

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,38 +11,41 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
-        const SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: const Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                child: Container(
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+          const SizedBox(
+            height: 4,
+          ),
+          SizedBox(
+            height: 60,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Colors.lightGreen,
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: const Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ),
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.lightGreen,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        Text(WeekDayTranslator().translate(label))
-      ],
+          Text(WeekDayTranslator().translate(label))
+        ],
+      ),
     );
   }
 }
