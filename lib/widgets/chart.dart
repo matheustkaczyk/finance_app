36ecac20d636990d65247f613cda5cc52c59f795
matheustkaczyk <matheus111904@hotmail.com>
2023-01-19 11:07:01 -0300

    feat: rendering ChartBars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:finance_app/models/transaction.dart';
+import 'package:finance_app/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -39,7 +40,20 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      child: Row(children: []),
+      child: Row(children: [
+        ...groupedTransactionsValues.map((data) {
+          return Flexible(
+            fit: FlexFit.tight,
+            child: ChartBar(
+              data['day'] as String,
+              data['value'] as double,
+              totalSpending == 0.0
+                  ? 0.0
+                  : (data['value'] as double) / totalSpending,
+            ),
+          );
+        }).toList(),
+      ]),
     );
   }
 }
