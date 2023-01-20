a91b4a86d8a628749ff805e237a394b1f98067b1
matheustkaczyk <matheus111904@hotmail.com>
2023-01-20 10:46:44 -0300

    refact: changing transaction list to listtile, converting to stateless widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:finance_app/widgets/not_found.dart';
 import '../models/transaction.dart';
 
-class TransactionsList extends StatefulWidget {
+class TransactionsList extends StatelessWidget {
   final List<Transaction> transactions;
+  Function deleteTransaction;
 
-  TransactionsList(this.transactions);
+  TransactionsList(this.transactions, this.deleteTransaction);
 
-  @override
-  State<TransactionsList> createState() => _TransactionsListState();
-}
-
-class _TransactionsListState extends State<TransactionsList> {
   String dateFormatter(String date) {
     List<String> dateList = date.toString().split(' ');
     String day = dateList[0];
@@ -23,19 +19,15 @@ class _TransactionsListState extends State<TransactionsList> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 600,
-      child: widget.transactions.isEmpty == true
+      child: transactions.isEmpty == true
           ? NotFound('Nenhum registro encontrado ainda!')
           : ListView.builder(
-              itemCount: widget.transactions.length,
+              itemCount: transactions.length,
               itemBuilder: (context, index) {
-                final transaction = widget.transactions[index];
+                final transaction = transactions[index];
                 return Dismissible(
                   key: UniqueKey(),
-                  onDismissed: (direction) => {
-                    setState(() {
-                      widget.transactions.removeAt(index);
-                    })
-                  },
+                  onDismissed: (direction) => deleteTransaction(transaction.id),
                   child: Card(
                     color: Colors.lightBlue,
                     margin: const EdgeInsets.all(8),
@@ -43,46 +35,40 @@ class _TransactionsListState extends State<TransactionsList> {
                       borderRadius: BorderRadius.circular(15.0),
                     ),
                     elevation: 4,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(left: 15),
+                    child: ListTile(
+                        leading: Container(
+                          padding: const EdgeInsets.all(10),
                           width: 100,
                           child: FittedBox(
                             child: Text(
                               "R\$ ${transaction.value.toStringAsFixed(2)}",
                               style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                  color: Colors.amberAccent),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.amberAccent,
+                              ),
                             ),
                           ),
                         ),
-                        Container(
-                            padding: const EdgeInsets.all(10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  transaction.title,
-                                  style: const TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                                Text(
-                                  dateFormatter(transaction.date.toString()),
-                                  style: const TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                              ],
-                            )),
-                      ],
-                    ),
+                        title: Text(
+                          transaction.title.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        subtitle: Text(
+                          dateFormatter(transaction.date.toString()),
+                          style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () => deleteTransaction(transaction.id),
+                        )),
                   ),
                 );
               },
